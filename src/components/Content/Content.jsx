b4791d7347597d5ab8566deb81ec86753bce6b08
marimarichka <mari128marichka@gmail.com>
2022-11-12 13:48:02 -0800

    add todos creating

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem/TodoItem";
 import s from "./Content.module.css";
 
 const Content = () => {
-  const todos = ['Todo 1', 'Todo 2', 'Todo 3'];
+  const [todos, setTodos] = useState([]);
+
+  const [value, setValue] = useState('');
+
+  const onInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onClick = () => {
+    setTodos([...todos, value]);
+    setValue('');
+  };
+
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
 
   return (
     <div className={s.content}>
       <div className={s.allContent}>
         <div className={s.todoTitle}>
-          <input type = 'text' />
-          <button>Add</button>
+          <input type='text' onKeyDown={onKeyDown} value={value} onChange={onInputChange} />
+          <button onClick={onClick}>Add</button>
         </div>
         {todos.map(name => <TodoItem name={name} />)}
       </div>
