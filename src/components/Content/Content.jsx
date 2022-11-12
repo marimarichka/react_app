2cca576b9a73a2e0a3041f4cc670696652a39da6
marimarichka <mari128marichka@gmail.com>
2022-11-12 15:34:13 -0800

    add overflow

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -5,7 +5,7 @@ import s from "./Content.module.css";
 const Content = () => {
   const [todos, setTodos] = useState([]);
 
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   const onInputChange = (e) => {
     setValue(e.target.value);
@@ -14,14 +14,14 @@ const Content = () => {
   const onClick = () => {
     const newTodo = {
       value,
-      id: new Date().getTime()
+      id: new Date().getTime(),
     };
     setTodos([...todos, newTodo]);
-    setValue('');
+    setValue("");
   };
 
   const onKeyDown = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       onClick();
     }
   };
@@ -35,16 +35,14 @@ const Content = () => {
     <div className={s.content}>
       <div className={s.allContent}>
         <div className={s.todoTitle}>
-          <input type='text' onKeyDown={onKeyDown} value={value} onChange={onInputChange} />
+          <input type="text" onKeyDown={onKeyDown} value={value} onChange={onInputChange} />
           <button onClick={onClick}>Add</button>
         </div>
-        {todos.map((todo, i) => (
-          <TodoItem 
-            name={todo.value} 
-            number={i + 1} 
-            deleteTodo={() => deleteTodo(todo.id)} 
-          />
-        ))}
+        <div className={s.todoList}>
+          {todos.map((todo, i) => (
+            <TodoItem name={todo.value} number={i + 1} deleteTodo={() => deleteTodo(todo.id)} />
+          ))}
+        </div>
       </div>
     </div>
   );
